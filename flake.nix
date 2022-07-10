203dc2bb2df6e411936beec5ae2b49ccf67fcdeb
libjared <libjared@users.noreply.github.com>
2022-07-10 18:19:15 -0500

    Add comments

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
   inputs = {
+    # use nixpkgs revision with the last nonbroken build of yi-core
     nixpkgs.url = "nixpkgs/0026c79c5509d9d170e85cab71ed7c256b4b0c7b";
   };
 
@@ -14,6 +15,7 @@
         pkgs."${set}".developPackage {
           inherit returnShellEnv;
           name = "yi-custom";
+          # ./yi-config is just a copy of github:yi/example-configs/yi-vim-vty-static/
           root = ./yi-config;
           modifier = drv: (
             pkgs.haskell.lib.addBuildTools drv (with pkgs."${set}"; if returnShellEnv then [
