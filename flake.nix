67e14179289e046b7fd91c9a44eba2c0cf90efb6
libjared <libjared@users.noreply.github.com>
2022-07-10 18:33:48 -0500

    Fix system always being x86_64-linux

--- flake.nix
+++ flake.nix
@@ -5,30 +5,30 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-      set = "haskellPackages";
-      project = returnShellEnv: (
-        pkgs."${set}".developPackage {
-          inherit returnShellEnv;
-          name = "yi-custom";
-          # ./yi-config is just a copy of github:yi/example-configs/yi-vim-vty-static/
-          root = ./yi-config;
-          modifier = drv: (
-            pkgs.haskell.lib.addBuildTools drv (with pkgs."${set}"; if returnShellEnv then [
-              cabal-install
-            ] else [ ])
-          );
-        }
-      );
-      yi-custom = project false;
-      yi-custom-dev = project true;
-    in
-    flake-utils.lib.eachDefaultSystem (system: {
-      defaultPackage = yi-custom;
-      devShell = yi-custom-dev;
-    });
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        set = "haskellPackages";
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        project = returnShellEnv: (
+          pkgs."${set}".developPackage {
+            inherit returnShellEnv;
+            name = "yi-custom";
+            # ./yi-config is just a copy of github:yi/example-configs/yi-vim-vty-static/
+            root = ./yi-config;
+            modifier = drv: (
+              pkgs.haskell.lib.addBuildTools drv (with pkgs."${set}"; if returnShellEnv then [
+                cabal-install
+              ] else [ ])
+            );
+          }
+        );
+        yi-custom = project false;
+        yi-custom-dev = project true;
+      in
+      {
+        defaultPackage = yi-custom;
+        devShell = yi-custom-dev;
+      });
 }
